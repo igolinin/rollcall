d55301f4a6022b93c0251cdf27a52a2f0f0d41bc
Ogyun Dzhambazov <ogyun95@gmail.com>
2018-05-06 17:32:34 +0200

    Homepage

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { TeacherDashboardComponent } from './components/teacher-dashboard/teacher-dashboard.component';
 import { TeacherLoginComponent } from './components/teacher-login/teacher-login.component';
+import{HomeComponent} from './components/home/home.component';
 
-
+const appRoutes: Routes = [
+   {path: '', component: HomeComponent, pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
     TeacherDashboardComponent,
     TeacherLoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
